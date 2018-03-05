99d1dde9ba38f61214ec77e1de3875f6d3e6dbfe
Rian Long <rian@rianlong.me>
2018-03-04 16:56:11 -0800

    updated website

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(function() {
+  //ScrollReveal
+  window.sr = ScrollReveal();
+  sr.reveal(".project", { duration: 500, viewFactor: 0.3 });
+  sr.reveal(".content-img", { duration: 500, viewFactor: 0.2 });
+  sr.reveal(".content-half-half", { duration: 500, viewFactor: 0.2 });
+});

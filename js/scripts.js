89c4b3bb610f6272078ea1eefc784d19e937bb49
Rian Long <rian@rianlong.me>
2018-03-05 09:32:33 -0800

    added scroll to top

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,23 @@ $(function() {
   sr.reveal(".project", { duration: 500, viewFactor: 0.3 });
   sr.reveal(".content-img", { duration: 500, viewFactor: 0.2 });
   sr.reveal(".content-half-half", { duration: 500, viewFactor: 0.2 });
+
+  // ===== Scroll to Top ====
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      // If page is scrolled more than 50px
+      $("#return-to-top").fadeIn(200); // Fade in the arrow
+    } else {
+      $("#return-to-top").fadeOut(200); // Else fade out the arrow
+    }
+  });
+  $("#return-to-top").click(function() {
+    // When arrow is clicked
+    $("body,html").animate(
+      {
+        scrollTop: 0 // Scroll to top of body
+      },
+      500
+    );
+  });
 });
